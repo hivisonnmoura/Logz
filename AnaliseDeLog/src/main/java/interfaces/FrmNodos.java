58bdec073fad9c84274716c6046f3c4a6ecb3d37
Tassia <ta_ta_martins@hotmail.com>
2016-11-18 14:28:45 -0200

    Removi comentarios desnecessarios

--- AnaliseDeLog/src/main/java/interfaces/FrmNodos.java
+++ AnaliseDeLog/src/main/java/interfaces/FrmNodos.java
@@ -1,6 +1,5 @@
 package interfaces;
 
-
 import objetodevalor.OVNoProcesso;
 import servicos.ServicoFachada;
 import servicos.ServicoTratamentoDeExcecaoSelecionaProcesso;
@@ -19,138 +18,132 @@ import java.io.Serializable;
 
 public class FrmNodos extends JFrame implements Serializable {
 
-    private JTable tableNodosCriticos;
-    private JTextField textField;
-    private JTextField textField_1;
-    private DadoTableModel tableModel;
-    private String caminhoDiretorio;
+	private JTable tableNodosCriticos;
+	private JTextField textField;
+	private JTextField textField_1;
+	private DadoTableModel tableModel;
+	private String caminhoDiretorio;
 
-    private ServicoFachada servicoFachada = new ServicoFachada();
+	private ServicoFachada servicoFachada = new ServicoFachada();
 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            try {
-                FrmNodos frame = new FrmNodos();
-                frame.setVisible(true);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-    }
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			try {
+				FrmNodos frame = new FrmNodos();
+				frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
 
-    public FrmNodos() {
-        setResizable(false);
-        setTitle("Logz - Resumo de pontos cr\u00EDticos");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 605, 439);
-        JPanel contentPane = new JPanel();
-        contentPane.setToolTipText("");
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
+	public FrmNodos() {
+		setResizable(false);
+		setTitle("Logz - Resumo de pontos cr\u00EDticos");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 605, 439);
+		JPanel contentPane = new JPanel();
+		contentPane.setToolTipText("");
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
-        setLocationRelativeTo(null);
+		setLocationRelativeTo(null);
 
-        JPanel panelPlanilha = new JPanel();
-        panelPlanilha.setLayout(null);
-        panelPlanilha.setBorder(new TitledBorder(null, "Relatorio dos pontos criticos", TitledBorder.LEADING,
-                TitledBorder.TOP, null, null));
-        panelPlanilha.setBounds(10, 11, 579, 364);
-        contentPane.add(panelPlanilha);
+		JPanel panelPlanilha = new JPanel();
+		panelPlanilha.setLayout(null);
+		panelPlanilha.setBorder(new TitledBorder(null, "Relatorio dos pontos criticos", TitledBorder.LEADING,
+				TitledBorder.TOP, null, null));
+		panelPlanilha.setBounds(10, 11, 579, 364);
+		contentPane.add(panelPlanilha);
 
-        JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setBounds(10, 21, 559, 332);
-        panelPlanilha.add(scrollPane);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 21, 559, 332);
+		panelPlanilha.add(scrollPane);
 
-        MouseListener mouseClick = new MouseListener() {
+		MouseListener mouseClick = new MouseListener() {
 
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                // TODO Auto-generated method stub
+			@Override
+			public void mouseReleased(MouseEvent e) {
 
-            }
+			}
 
-            @Override
-            public void mousePressed(MouseEvent e) {
-                // TODO Auto-generated method stub
+			@Override
+			public void mousePressed(MouseEvent e) {
 
-            }
+			}
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-                // TODO Auto-generated method stub
+			@Override
+			public void mouseExited(MouseEvent e) {
 
-            }
+			}
 
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                // TODO Auto-generated method stub
+			@Override
+			public void mouseEntered(MouseEvent e) {
 
-            }
+			}
 
-            @Override
-            public void mouseClicked(MouseEvent e) {
+			@Override
+			public void mouseClicked(MouseEvent e) {
 
-            }
-        };
+			}
+		};
 
-        tableNodosCriticos = new JTable();
-        tableNodosCriticos.setColumnSelectionAllowed(true);
-        tableNodosCriticos.setAutoCreateRowSorter(true);
-        tableNodosCriticos.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-        tableNodosCriticos.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent arg0) {
-                tableNodosCriticos.addColumnSelectionInterval(0, 9);
-            }
-        });
+		tableNodosCriticos = new JTable();
+		tableNodosCriticos.setColumnSelectionAllowed(true);
+		tableNodosCriticos.setAutoCreateRowSorter(true);
+		tableNodosCriticos.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		tableNodosCriticos.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				tableNodosCriticos.addColumnSelectionInterval(0, 9);
+			}
+		});
 
-        scrollPane.setViewportView(tableNodosCriticos);
-        tableNodosCriticos.setModel(new DadoTableModel(OVNoProcesso.criarCom(servicoFachada.retornaListaEntidadeNo())));
-        tableNodosCriticos.setBorder(new LineBorder(Color.LIGHT_GRAY));
-        tableNodosCriticos.setColumnSelectionAllowed(true);
-        tableNodosCriticos.setCellSelectionEnabled(true);
-        tableNodosCriticos.getColumnModel().getColumn(1).setPreferredWidth(120);
-        tableNodosCriticos.getColumnModel().getColumn(0).setPreferredWidth(120);
-        tableNodosCriticos.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		scrollPane.setViewportView(tableNodosCriticos);
+		tableNodosCriticos.setModel(new DadoTableModel(OVNoProcesso.criarCom(servicoFachada.retornaListaEntidadeNo())));
+		tableNodosCriticos.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		tableNodosCriticos.setColumnSelectionAllowed(true);
+		tableNodosCriticos.setCellSelectionEnabled(true);
+		tableNodosCriticos.getColumnModel().getColumn(1).setPreferredWidth(120);
+		tableNodosCriticos.getColumnModel().getColumn(0).setPreferredWidth(120);
+		tableNodosCriticos.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
+		JButton btnPrximo = new JButton("Proximo");
+		btnPrximo.addActionListener(arg0 -> {
+			try {
+				caminhoDiretorio = tableNodosCriticos.getModel().getValueAt(tableNodosCriticos.getSelectedRow(), 9)
+						.toString();
+				String caminhoCpuDetalhado = servicoFachada.direcionaCPUProcess(caminhoDiretorio);
+				ProcessaDadosCpuDetalhado.processaCpuDetalhada(caminhoCpuDetalhado);
+				String caminhoCpuCohere = servicoFachada.direcionaCpuCohere(caminhoDiretorio);
+				ProcessaDadosCpuCohere.processaCpuCohere(caminhoCpuCohere);
 
-        JButton btnPrximo = new JButton("Proximo");
-        btnPrximo.addActionListener(arg0 -> {
-            try {
-                caminhoDiretorio = tableNodosCriticos.getModel().getValueAt(tableNodosCriticos.getSelectedRow(), 9)
-                        .toString();
-                String caminhoCpuDetalhado = servicoFachada.direcionaCPUProcess(caminhoDiretorio);
-                ProcessaDadosCpuDetalhado.processaCpuDetalhada(caminhoCpuDetalhado);
-                String caminhoCpuCohere = servicoFachada.direcionaCpuCohere(caminhoDiretorio);
-                ProcessaDadosCpuCohere.processaCpuCohere(caminhoCpuCohere);
+				FrmStack frmStack = new FrmStack();
+				frmStack.setVisible(true);
+				setVisible(false);
+			} catch (ArrayIndexOutOfBoundsException ex) {
+				ServicoTratamentoDeExcecaoSelecionaProcesso servicoTratamentoDeExcecaoSelecionaProcesso = new ServicoTratamentoDeExcecaoSelecionaProcesso();
+				servicoTratamentoDeExcecaoSelecionaProcesso.tratamentoExcecaoSelecionaNo();
+			}
 
-                FrmStack frmStack = new FrmStack();
-                frmStack.setVisible(true);
-                setVisible(false);
-            } catch (ArrayIndexOutOfBoundsException ex) {
-                ServicoTratamentoDeExcecaoSelecionaProcesso servicoTratamentoDeExcecaoSelecionaProcesso = new ServicoTratamentoDeExcecaoSelecionaProcesso();
-                servicoTratamentoDeExcecaoSelecionaProcesso.tratamentoExcecaoSelecionaNo();
-            }
+		});
+		btnPrximo.setBounds(500, 377, 89, 23);
+		contentPane.add(btnPrximo);
 
-        });
-        btnPrximo.setBounds(500, 377, 89, 23);
-        contentPane.add(btnPrximo);
+		JButton btnRetornar = new JButton("Retornar");
+		btnRetornar.addActionListener(arg0 -> {
+			servicoFachada.deletaRepositorios();
+			FrmDiretorio frmDiretorio = new FrmDiretorio();
+			frmDiretorio.setVisible(true);
+			setVisible(false);
 
-        JButton btnRetornar = new JButton("Retornar");
-        btnRetornar.addActionListener(arg0 -> {
-            servicoFachada.deletaRepositorios();
-            FrmDiretorio frmDiretorio = new FrmDiretorio();
-            frmDiretorio.setVisible(true);
-            setVisible(false);
+		});
+		btnRetornar.setBounds(10, 377, 89, 23);
+		contentPane.add(btnRetornar);
+		JLabel lblSelecioneOProcesso = new JLabel("Selecione o processo desejado para prosseguir");
+		lblSelecioneOProcesso.setBounds(170, 381, 297, 14);
+		contentPane.add(lblSelecioneOProcesso);
 
-
-        });
-        btnRetornar.setBounds(10, 377, 89, 23);
-        contentPane.add(btnRetornar);
-        JLabel lblSelecioneOProcesso = new JLabel("Selecione o processo desejado para prosseguir");
-        lblSelecioneOProcesso.setBounds(170, 381, 297, 14);
-        contentPane.add(lblSelecioneOProcesso);
-
-    }
+	}
 }
